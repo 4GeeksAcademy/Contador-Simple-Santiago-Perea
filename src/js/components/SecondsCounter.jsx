1d08ac3127075033d8858834c28c97a4c861670c
santiago-pp <santiagopereaap@gmail.com>
2025-11-14 19:05:06 +0000

    v2

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,12 +1,15 @@
 export default function SecondsCounter(props) { 
-    //const seconds = Math.floor(props.seconds % 60);
+    const seconds = String(Math.floor(props.seconds % 60)).padStart(2, "0");
+    const minutes = String(Math.floor((props.seconds % 3600) / 60)).padStart(2, "0");
+    const hours = String(Math.floor((props.seconds % 86400) / 3600)).padStart(2, "0");
+    const days = String(Math.floor(props.seconds / 86400)).padStart(2, "0");
     return (
     <div className="counter">
       <div className="calendar"><i className="fa-regular fa-clock"></i></div>
-      <div className="days">{String(Math.floor(props.seconds / 86400)).padStart(2, "0")} :</div>
-      <div className="hours">{String(Math.floor((props.seconds % 86400) / 3600)).padStart(2, "0")} :</div>
-      <div className="minutes">{String(Math.floor((props.seconds % 3600) / 60)).padStart(2,"0")} :</div>
-      <div className="seconds">{String(Math.floor(props.seconds % 60)).padStart(2,"0")}</div>
+      <div className="days">{days} :</div>
+      <div className="hours">{hours} :</div>
+      <div className="minutes">{minutes} :</div>
+      <div className="seconds">{seconds}</div>
     </div>
     )
 }
